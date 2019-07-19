f93a6aa193f7bbd533731df4812f0b817ab63845
yurii <anthousedev@gmail.com>
2019-07-20 00:05:28 +0300

    for dimon 228

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainPageComponent} from './pages/components/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'landing', component: MainPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
